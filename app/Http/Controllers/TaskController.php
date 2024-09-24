e727da603d48166682af065f5b44939107ba07b6
kumodarika <kumoda.rika@revamp.co.jp>
2024-09-24 14:23:30 +0900

    Requestクラス作成によるバリデーション機能(create)の実装

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\createRequest;
 use App\Models\Task;
 
 class TaskController extends Controller
@@ -17,31 +18,24 @@ class TaskController extends Controller
         return view('todolist.task_create');
     }
 
-    public function create(Request $request)
+    public function create(createRequest $request)
     {
-        //バリデーション
-        $validated = $request->validate([
-            'status' => 'required|integer|in:1,2,3',
-            'title' => 'required|string|max:100',
-            'due_date' => 'nullable|date|after_or_equal:now',
-            'assignee' => 'required|string|max:20',
-        ],
-        [
-            'status.required' => 'ステータスは必須です。',
-            'title.required' => 'タイトルは必須です。',
-            'due_date.after_or_equal' => '期日は今日以降の日付でなければなりません。',
-            'assignee.required' => '担当者は必須です。',
-        ]);
+        // バリデーション済みデータを取得
+        try {
+        $data = $request->validated();
+
             $task = new Task();
-            $task->status = $request -> status ;
-            $task->title = $request -> title ;
-            $task->due_date = $request -> due_date ;
-            $task->assignee = $request -> assignee ;
+            $task->status = $request->status;
+            $task->title = $request->title;
+            $task->due_date = $request->due_date;
+            $task->assignee = $request->assignee;
             $task->save();
 
-            return redirect('/');
+            return redirect('/')->with('success', 'タスクが作成されました。');
+    }catch (\Exception $e) {
+        dd($e->getMessage()); // エラー内容を表示
     }
-
+    }
     public function editTask($id)
     {
         $task = Task::find($id);
